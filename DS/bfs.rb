2f665e12dfb14872fd280af4cf1eabf2a91f3501
Manoj <manoj.delhi24@gmail.com>
2017-10-22 22:55:25 +0530

    new test input added for bfs

--- DS/bfs.rb
+++ DS/bfs.rb
@@ -43,17 +43,24 @@ end
 
 graph = Graph.new
 
+# graph.add(0, [1,2])
+# graph.add(1, [0,3])
+# graph.add(2, [0,3,4])
+# graph.add(3, [1,2,5])
+# graph.add(4, [2])
+# graph.add(5, [3])
+
 graph.add(0, [1,2])
-graph.add(1, [0,3])
-graph.add(2, [0,3,4])
-graph.add(3, [1,2,5])
-graph.add(4, [2])
-graph.add(5, [3])
+graph.add(1, [0, 2, 3])
+graph.add(2, [0,1,3])
+graph.add(3, [1,2])
+
 
 Bfs.new(graph, 0).call
 
+# debugger
+# true
 
 
-debugger
-true
+
 
