290168430f002e9aa664885cf54a1446d52f151e
Manoj <manoj.delhi24@gmail.com>
2017-12-18 09:32:18 +0530

    merge 2 sorted lists with recurssion implementation

---
+++ Linklist/merge_recurr.rb
@@ -0,0 +1,51 @@
+require "byebug"
+Node = Struct.new(:item, :next)
+
+def merge(a, b)
+  result = nil
+
+  return b if a.nil?
+  return a if b.nil?
+
+  if a.item <= b.item
+    result = a
+    result.next = merge(a.next, b)
+  else
+    result = b
+    result.next = merge(a, b.next)
+  end
+  return result
+end
+
+l1 = Node.new(10, nil)
+l2 = Node.new(20, nil)
+l3 = Node.new(30, nil)
+l4 = Node.new(40, nil)
+l5 = Node.new(50, nil)
+l6 = Node.new(60, nil)
+
+l1.next = l2
+l2.next = l3
+l3.next = l4
+l4.next = l5
+l5.next = l6
+
+m1 = Node.new(5, nil)
+m2 = Node.new(22, nil)
+m3 = Node.new(25, nil)
+m4 = Node.new(32, nil)
+m1.next = m2
+m2.next = m3
+m3.next = m4
+
+result = merge(l1, m1)
+
+puts "Printing Result"
+curr = result
+while !curr.nil?
+  print "#{curr.item} -> "
+  curr = curr.next
+end
+
+# debugger
+# true
