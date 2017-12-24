86474c664040c9ea41c7bc68a529ad060399a4f4
Manoj <manoj.delhi24@gmail.com>
2017-12-24 07:20:53 +0530

    reverse list recursion definition modified

--- Linklist/reverse_recur.rb
+++ Linklist/reverse_recur.rb
@@ -1,17 +1,43 @@
 require "byebug"
 
 Node = Struct.new(:item, :next)
-def reverse(node, start)
-  unless node.next
-    return [node, node]
+# def reverse(node, start)
+#   unless node.next
+#     return [node, node]
+#   end
+
+#   _next, _start = reverse(node.next, start)
+#   _next.next = node
+#   puts "#{node.item}, #{start.item}"
+#   node.next = nil if node.item == start.item
+#   return node, _start
+# end
+
+def print_list(node)
+  curr = node
+  items = []
+  while curr != nil
+    items.push curr.item
+    curr = curr.next
+  end
+  puts items.join(' -> ')
+end
+
+def reverse(node)
+
+  if node.next == nil || node == nil
+    return node # returning head
   end
 
-  _next, _start = reverse(node.next, start)
-  _next.next = node
-  puts "#{node.item}, #{start.item}"
-  node.next = nil if node.item == start.item
-  return node, _start
+  head = reverse(node.next)
+
+  node.next.next = node
+  node.next = nil
+  return head
+
 end
+
+
 
 
 n1 = Node.new(1, nil)
@@ -25,6 +51,11 @@ n2.next = n3
 n3.next = n4
 n4.next = n5
 
-_end, start = reverse(n1, n1)
-debugger
-true
+puts "Old list: "
+print_list n1
+head = reverse(n1)
+puts "New list: "
+print_list head
+# _end, start = reverse(n1, n1)
+# debugger
+# true
