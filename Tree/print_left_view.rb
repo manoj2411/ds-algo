7db881410195d7701f36cf7d0c3a8e9e559007f0
Manoj <manoj.delhi24@gmail.com>
2017-12-27 09:06:44 +0530

    print left view improvisation without using array

--- Tree/print_left_view.rb
+++ Tree/print_left_view.rb
@@ -11,6 +11,19 @@ def print_left_view(node, arr, level)
   print_left_view(node.right, arr, level + 1) if node.right
 end
 
+@max_level = -1
+def print_left_view_new(node, level)
+  return if node.nil?
+
+  if @max_level < level
+    print " #{node.key} "
+    @max_level = level
+  end
+  print_left_view_new(node.left, level + 1) if node.left
+  print_left_view_new(node.right, level + 1) if node.right
+end
+
+
 # Node.new()
 
 root = Node.new(4)
@@ -23,4 +36,4 @@ root.right.right.left.right = Node.new(22)
 root.right.left.left = Node.new(6)
 root.right.left.right = Node.new(7)
 
-print_left_view(root, [], 0)
+print_left_view_new(root, 0)
