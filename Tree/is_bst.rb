0a5475dfc6d1a4df7de2b9196ab8b7e85f5049fb
Manoj <manoj.delhi24@gmail.com>
2017-12-27 22:00:10 +0530

    is BST implementation corrected

--- Tree/is_bst.rb
+++ Tree/is_bst.rb
@@ -1,29 +1,62 @@
 Node = Struct.new(:key, :left, :right)
 
-def is_bst?(node)
+# Wrong implementation
+# def is_bst?(node)
+#   return true if node == nil
+
+#   left = node.left
+#   right = node.right
+#   result = true
+
+#   if left && left.key > node.key
+#     result = false
+#   elsif right && right.key < node.key
+#     result = false
+#   end
+
+#   return result && is_bst?(left) && is_bst?(right)
+# end
+
+def isBSTUtil?(node, min, max)
   return true if node == nil
+  return false if node.key < min || node.key > max
 
-  left = node.left
-  right = node.right
-  result = true
-
-  if left && left.key > node.key
-    result = false
-  elsif right && right.key < node.key
-    result = false
+  if node.key < min || node.key > max
+    return false
   end
 
-  return result && is_bst?(left) && is_bst?(right)
+  return isBSTUtil?(node.left, min, node.key) &&
+         isBSTUtil?(node.right, node.key - 1, max)
+
 end
 
-root = Node.new(4)
-root.left = Node.new(2)
-root.right = Node.new(5)
-# root.right.left = Node.new(6)
-root.left.left = Node.new(1)
-root.left.right = Node.new(3)
+  MIN = -99999
+  MAX = 99999
+def isBst?(node)
+
+  return isBSTUtil?(node, MIN, MAX)
+end
+
+# root = Node.new(13)
+# root.left = Node.new(2)
+# root.right = Node.new(15)
+# # root.right.left = Node.new(6)
+# root.left.left = Node.new(1)
+# root.left.right = Node.new(4)
+
+# root = Node.new(4);
+# root.left = Node.new(2);
+# root.right = Node.new(5);
+# root.left.left = Node.new(1);
+# root.left.right = Node.new(3)
+
+root = Node.new(3);
+root.left        = Node.new(2);
+root.right       = Node.new(5);
+root.left.left  = Node.new(1);
+root.left.right = Node.new(4)
 
 
-puts "is BTS: #{is_bst?(root)}"
+puts "is BTS: #{isBst?(root)}"
 
 
