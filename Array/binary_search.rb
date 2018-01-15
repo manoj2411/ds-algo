ec7601a6f41588adf257ececf56202e5f4c81cc7
Manoj <manoj.delhi24@gmail.com>
2018-01-15 22:23:24 +0530

    binary search recursive implementation

--- Array/binary_search.rb
+++ Array/binary_search.rb
@@ -14,6 +14,21 @@ def bsearch(arr, key)
   return nil
 end
 
+def bsearch_recur(arr, k, low, high)
+  if low <= high
+    mid = (low + high) / 2
+    if arr[mid] == k
+      return true
+    elsif k < arr[mid]
+      binary_search(arr, k, low, mid - 1)
+    else
+      binary_search(arr, k, mid + 1, high)
+    end
+  else
+    return false
+  end
+end
+
 arr = [2,5,8,12,16,23,38,56,72]
 key = 72
 puts "Array: #{arr}"
