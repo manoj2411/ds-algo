1812d22f762c265f2c1e97ba8bab0eeb649e5d82
Manoj <manoj.delhi24@gmail.com>
2018-01-27 09:41:21 +0530

    maximum possible stolen value from houses efficient implementation

--- Dp/max_possible_stolen_value_houses.py
+++ Dp/max_possible_stolen_value_houses.py
@@ -6,17 +6,21 @@ def max_stolen(lst, n):
     elif n == 2:
         max(lst)
 
-    res = [0] * n
-    res[0] = lst[0]
-    res[1] = max(lst[0], lst[1])
+    # res = [0] * n
+    # res[0] = lst[0]
+    # res[1] = max(lst[0], lst[1])
+    val2 = lst[0]
+    val1 = max(lst[0], lst[1])
     for i in range(2,n):
-        res[i] = max(lst[i] + res[i-2], res[i-1])
+        curr = max(lst[i] + val2, val1)
+        val2 = val1
+        val1 = curr
 
-    return res[-1]
+    return val1
 
 def main():
-    # lst = [6,7,1,3,8,2,4]
-    lst = [5,3,4,11,2]
+    lst = [6,7,1,3,8,2,4]
+    # lst = [5,3,4,11,2]
     n = len(lst)
     print("Max value stolen is: " + str(max_stolen(lst, n)) )
 
