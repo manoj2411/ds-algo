811a5404ea3eb3a92e3e49ced33c9b6eb65f0997
Manoj <manoj.delhi24@gmail.com>
2018-02-27 19:25:59 +0530

    Flatten a multi-level linked list, Depth wise implementation

---
+++ Linklist/flatten_depth_wise.rb
@@ -0,0 +1,61 @@
+Node = Struct.new(:key, :next, :child)
+
+def flatten(head)
+    arr = []
+    return flatten_util(head, arr)
+end
+
+def flatten_util(node, last)
+    if node == nil
+        return
+    end
+
+    last[0] = node
+    nxt = node.next
+
+    if node.child
+        node.next = flatten_util(node.child, last)
+    end
+
+    if nxt
+        last[0].next = flatten_util(nxt, last)
+    end
+    return node
+end
+
+def _p(head)
+    curr = head
+    while curr != nil
+        print "#{curr.key} "
+        curr = curr.next
+    end
+    puts
+end
+
+head = Node.new(1)
+head.next = Node.new(2)
+head.next.child = Node.new(7)
+head.next.child.child = Node.new(9)
+head.next.child.child.child = Node.new(14)
+head.next.child.child.child.child = Node.new(15)
+head.next.child.child.child.child.next = Node.new(23)
+head.next.child.child.child.child.next.child = Node.new(24)
+
+head.next.child.next = Node.new(8)
+head.next.child.next.child = Node.new(16)
+head.next.child.next.child.child = Node.new(17)
+head.next.child.next.child.child.next = Node.new(18)
+head.next.child.next.child.child.next.next = Node.new(19)
+head.next.child.next.child.child.next.next.next = Node.new(20)
+head.next.child.next.child.child.next.next.next.child = Node.new(21)
+
+head.next.child.next.next = Node.new(10)
+head.next.child.next.next.child = Node.new(11)
+head.next.child.next.next = Node.new(12)
+
+head.next.next = Node.new(3)
+head.next.next.next = Node.new(4)
+
+_p(head)
+head = flatten(head)
+_p(head)
