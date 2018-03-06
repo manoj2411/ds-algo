8828bb2f590bb30632131c7d1d849604707b857c
Manoj <manoj.delhi24@gmail.com>
2018-03-06 12:14:09 +0530

    Trapping Rain Water implementaion

---
+++ Array/trap_rain_water.py
@@ -0,0 +1,36 @@
+def trap_rain_water(arr):
+    l = 0
+    r = len(arr) - 1
+    return tw_util(arr, l, r)
+
+def tw_util(arr, l, r):
+    if r - l <= 1 :
+        return 0
+
+    mid = get_mid(arr, l, r)
+
+    if mid == -1 :
+        return calulate_water_inside(arr, l, r)
+    return calulate_water_inside(arr, l, mid) + calulate_water_inside(arr, mid, r)
+
+def get_mid(arr, l, r):
+    for i in range(l,r):
+        if (arr[i] > arr[l]) or (arr[i] > arr[r]) :
+            return i
+    return -1
+
+def calulate_water_inside(arr, l, r):
+    _min = min(arr[l], arr[r])
+    _sum = 0
+
+    for i in range(l + 1, r):
+        _sum += _min - arr[i]
+    return _sum
+
+
+lst = [[3,0,0,2,0,4], [6,5,7,4,0,9], [6,9,9]]
+
+for arr in lst:
+    print("List: {}".format(arr))
+    print("Trapped rain water: {}\n".format(trap_rain_water(arr)))
+
