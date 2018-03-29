7e6b37fd01ddd44451566dc5dedf5e14f7d72a8a
Manoj <manoj.delhi24@gmail.com>
2018-03-29 12:21:04 +0530

    form the biggest number from array new implementation added

--- Array/form_largest_number.rb
+++ Array/form_largest_number.rb
@@ -6,8 +6,22 @@ def form_largest_number(arr)
   end.join
 end
 
+def form_largest_number_new(arr)
+  max_length = arr.max.to_s.length
+  pairs = arr.map do |num|
+    pair = [num]
+    str = num.to_s
+    pair << (str * ((max_length / str.length) + 1))[0..max_length].to_i
+  end
+
+  pairs.sort {|a,b| b[1] <=> a[1] }.map {|lst| lst[0] }.join('')
+end
+
 
 [[1, 34, 3, 98, 9, 76, 45, 4, 121, 12], [12, 121], [121, 12]].each do |arr|
   puts "\nArr: #{arr}"
+  puts "Method1: "
   puts form_largest_number(arr)
+  puts "Method2: "
+  puts form_largest_number_new(arr)
 end
