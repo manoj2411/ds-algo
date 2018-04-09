6fa5632ddd316967b09ff6293726385841010914
Manoj <manoj.delhi24@gmail.com>
2018-04-09 07:24:47 +0530

    alternate logic added as coment for stack buy and sell problem

--- Array/stock_buy_n_sell.py
+++ Array/stock_buy_n_sell.py
@@ -1,5 +1,5 @@
 def stock_by_sell(arr):
-    if len(arr) <= 1
+    if len(arr) <= 1 :
         return
 
     end = start = 0
@@ -14,7 +14,26 @@ def stock_by_sell(arr):
     if start != end :
         print("({},{}) -- ({},{})".format(start, end, arr[start], arr[end]))
 
-lists = [[100,180,260,310,40,535,695], [23,13,25,29,33,19,34,45,65,67]]
+    # i = 0
+    # length = len(arr)
+    # while i < length - 1:
+    #     while i < length - 1 and arr[i] > arr[i + 1] :
+    #         i += 1
+
+    #     if i == length - 1 :
+    #         break
+
+    #     start = i
+
+    #     while i < (length - 1) and arr[i] < arr[i + 1]:
+    #         i += 1
+
+    #     print("({},{}) -- ({},{})".format(start, i, arr[start], arr[i]))
+    #     i += 1
+
+
+lists = [[100,180,260,310,40,535,695], [23,13,25,29,33,19,34,45,65,67],
+         [10,9,8,6,10,20,12,22]]
 
 for lst in lists :
     print("List: {}".format(lst))
