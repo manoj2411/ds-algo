aaff2fbf2e70e8680ee7a3f8b9f1a8073e74af8b
Manoj <manoj.delhi24@gmail.com>
2018-04-09 07:23:46 +0530

    spiral traversal code refactored

--- 2D/spiral_traversal.py
+++ 2D/spiral_traversal.py
@@ -5,15 +5,13 @@ def traverse_spirally(matrix):
 
     while rowl <= rowh and coll <= colh:
         # 1 - left to right
-        if rowl <= rowh:
-            for i in range(coll, colh + 1) :
-                print(matrix[rowl][i], end = ' ')
-            rowl += 1
+        for i in range(coll, colh + 1) :
+            print(matrix[rowl][i], end = ' ')
+        rowl += 1
         # 2 - top to down
-        if coll <= colh :
-            for i in range(rowl, rowh + 1):
-                print(matrix[i][colh], end = ' ')
-            colh -= 1
+        for i in range(rowl, rowh + 1):
+            print(matrix[i][colh], end = ' ')
+        colh -= 1
         # 3 - right to left
         if rowl <= rowh :
             for i in range(colh, coll - 1, -1):
@@ -25,8 +23,6 @@ def traverse_spirally(matrix):
                 print(matrix[i][coll], end = ' ')
             coll += 1
     print('')
-
-
 
 
 matrix = [
