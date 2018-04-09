a2b25e66a6b7aa8dcecdcbee3b45735ca2b45f66
Manoj <manoj.delhi24@gmail.com>
2018-04-09 16:45:46 +0530

    detect cycle in directed graph using DFS implementation corrected

--- Graphs/detect_cycle_in_directed_graph.rb
+++ Graphs/detect_cycle_in_directed_graph.rb
@@ -1,52 +1,52 @@
-require "pry"
-
 class Graph
-  attr_reader :vertices
-
+  attr_reader :matrix
   def initialize(size)
-    @vertices = {}
-    build_vertices(size)
+    @matrix = Array.new(size) { Array.new(size, 0)}
   end
 
   def add_edge(u,v)
-    @vertices[u] << v
+    matrix[u][v] = 1
   end
 
-  def relate_vertices(vertex)
-    @vertices[vertex]
+  def print
+    matrix.each {|row| puts row.join(', ')}
   end
 
   def has_cycle?
-    visited = Array.new @vertices.keys.length
-    in_stack = Array.new @vertices.keys.length
+    visited = Array.new(matrix.size, false)
+    in_stack = Array.new(matrix.size, false)
 
-    @vertices.keys.each do |vertex|
-      next if visited[vertex] || in_stack[vertex]
-
-      # initialize stack, base data
-      stack = [vertex]
-      in_stack[stack.last] = true
-      while not stack.empty?
-        curr_vertex = stack.pop
-        visited[curr_vertex] = true
-        in_stack[curr_vertex] = false
-        relate_vertices(curr_vertex).each do |related_vertex|
-          if visited[related_vertex] || in_stack[related_vertex]
-            return true
-          end
-          stack << related_vertex
-          in_stack[related_vertex] = true
+    for i in 0...matrix.length
+      if visited[i] == false
+        if has_cycle_helper?(i, visited, in_stack)
+          return true
         end
       end
     end
-
     return false
   end
 
-  private
-    def build_vertices(size)
-      size.times {|i| @vertices[i] = []}
+  def related_vertices(v)
+    vertices = []
+    matrix[v].each_with_index do |value, indx|
+      vertices << indx if value == 1
     end
+    return vertices
+  end
+
+  private
+    def has_cycle_helper?(v, visited, in_stack)
+      visited[v] = true
+      in_stack[v] = true
+      related_vertices(v).each do |rel_vertex|
+        if in_stack[rel_vertex] || has_cycle_helper?(rel_vertex, visited, in_stack)
+          return true
+        end
+      end
+      in_stack[v] = false
+      return false
+    end
+
 end
 
 ### Tests
@@ -55,9 +55,9 @@ end
 # graph.add_edge(0,1)
 # graph.add_edge(0,2)
 # graph.add_edge(1,2)
-# graph.add_edge(2,0)
+# # graph.add_edge(2,0)
 # graph.add_edge(2,3)
-# graph.add_edge(3,3)
+# # graph.add_edge(3,3)
 
 # graph = Graph.new(3)
 # graph.add_edge 2,2
@@ -68,7 +68,9 @@ end
 
 graph = Graph.new(4)
 graph.add_edge 0, 1
+graph.add_edge 0, 2
 graph.add_edge 1, 2
+graph.add_edge 1, 3
 graph.add_edge 2, 3
 
 puts "Cycle detected? - #{graph.has_cycle?}"
