222deb871dd98fd72966ccaa800d9cc7197eb40f
Manoj <manoj.delhi24@gmail.com>
2018-05-05 18:41:53 +0530

    sudoku code refactored

--- 2D/sudoku.rb
+++ 2D/sudoku.rb
@@ -1,44 +1,63 @@
-def solve_sudoku(m)
-  i, j = find_next_empty_space(m)
-  if i.nil?
+def solve_sudoku(matrix)
+  cell = find_next_empty_space(matrix)
+  if cell.empty?
     return true
   end
 
+  i, j = *cell
+
   for x in 1..9
-    if is_safe?(m, i, j, x)
-      m[i][j] = x
-      if solve_sudoku(m)
+    if is_safe?(matrix, i, j, x)
+      # choose
+      matrix[i][j] = x
+      # explore
+      if solve_sudoku(matrix)
         return true
       end
-      m[i][j] = 0
+      # unchoose
+      matrix[i][j] = 0
     end
   end
   return false
 end
 
-def find_next_empty_space(m)
+def find_next_empty_space(matrix)
   for i in 0...9
     for j in 0...9
-      return i,j if m[i][j] == 0
+      return [i,j] if matrix[i][j] == 0
     end
   end
-  return
+  return []
 end
 
-def is_safe?(m, i, j, num)
-  for k in 0...9
-    if m[i][k] == num || m[k][j] == num
+def is_safe?(matrix, r, c, x)
+  # check row
+  for i in 0..8
+    if matrix[i][c] == x
       return false
     end
   end
-  x = (i / 3) * 3
-  y = (j / 3) * 3
-
-  for k in x..(x + 2)
-    for l in y..(y + 2)
-      return false if m[k][l] == num
+  # check column
+  for i in 0..8
+    if matrix[r][i] == x
+      return false
     end
   end
+
+  # check box
+  # 0,1,2 = 0
+  # 3,4,5 = 3
+  # 6,7,8 = 6
+  row_i = (r / 3) * 3
+  col_i = (c / 3) * 3
+  for i in row_i..(row_i + 2)
+    for j in col_i..(col_i + 2)
+      if matrix[i][j] == x
+        return false
+      end
+    end
+  end
+
   return true
 end
 
