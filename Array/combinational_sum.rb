c8df66ab3814dcecd9cb88db11cfa46d37017af2
Manoj <manoj.delhi24@gmail.com>
2018-06-04 15:28:20 +0530

    combination sum code refactored

--- Array/combinational_sum.rb
+++ Array/combinational_sum.rb
@@ -1,23 +1,23 @@
 def combinational_sum(arr, sum)
   result = []
-  curr = []
-  helper(arr.sort, curr, result, sum, 0)
+  helper(arr.sort, [], result, sum, 0)
   return result.uniq
 end
 
 def helper(arr, curr, result, sum, i)
   if sum == 0
-    result << curr.clone
+    if !result.index(curr)
+      result << curr
+    end
     return
   end
 
   for j in i...arr.length
-    if sum - arr[j] < 0
-      return
-    end
-    curr.push(arr[j])
-    helper(arr, curr, result, sum - arr[j], j + 1)
-    curr.pop
+    return if arr[j] > sum
+
+    # curr.push(arr[j])
+    helper(arr, curr + [arr[j]], result, sum - arr[j], j + 1)
+    # curr.pop
   end
 end
 
