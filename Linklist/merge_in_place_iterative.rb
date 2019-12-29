3954fe4077490274ed764a712db589171b51b2ff
Manoj <manoj.delhi24@gmail.com>
2019-12-29 07:54:11 +0100

    Refactor: merge two sorted lists in place iterative Handle null cases as well.

--- Linklist/merge_in_place_iterative.rb
+++ Linklist/merge_in_place_iterative.rb
@@ -1,77 +1,65 @@
 Node = Struct.new(:item, :next)
 
 # Without using dummy node/extra space
-def merge_inplace(a,b)
-  if a.item <= b.item
-    list = a
-    extra = b
-  else
-    list = b
-    extra = a
+def merge_inplace(list_a, list_b)
+  if list_b.nil?
+    return list_a
+  elsif list_a.nil?
+    return list_b
+  elsif list_a.item > list_b.item
+    list_a, list_b = list_b, list_a
   end
+  # Now we are sure that list_a has smaller first element then list_b
 
-  prev = list
-  curr = list.next
+  _curr = list_a
+  _next = list_a.next
 
-  while extra != nil
-    if curr == nil
-      prev.next = extra
-      extra = nil
+  while list_b != nil
+    if _next == nil
+      _curr.next = list_b
+      list_b = nil
       break
     end
 
-    if curr.item <= extra.item
-      prev = curr
-      curr = curr.next
+    if _next.item <= list_b.item
+      _curr = _next
+      _next = _next.next
     else
-      prev.next = extra
-      extra = extra.next
-      prev = prev.next
-      prev.next = curr
+      _curr.next = list_b
+      list_b = list_b.next
+      _curr = _curr.next
+      _curr.next = _next
     end
   end
-  return list
+  list_a
 end
 
 def print_list(node)
-  curr = node
-  while !curr.nil?
-    print "#{curr.item} -> "
-    curr = curr.next
+  while not node.nil?
+    print "#{node.item} "
+    node = node.next
   end
   puts
 end
 
 
-l1 = Node.new(10, nil)
-l2 = Node.new(20, nil)
-l3 = Node.new(30, nil)
-l4 = Node.new(40, nil)
-l5 = Node.new(50, nil)
-l6 = Node.new(60, nil)
+head1 = Node.new(10)
+head1.next = Node.new(20)
+head1.next.next = Node.new(30)
+# head1.next.next.next = Node.new(40)
+# head1.next.next.next.next = Node.new(50)
+# head1.next.next.next.next.next = Node.new(60)
 
-l1.next = l2
-l2.next = l3
-l3.next = l4
-l4.next = l5
-l5.next = l6
+head2 = Node.new(5)
+head2.next = Node.new(22)
+# head2.next.next = Node.new(25)
+# head2.next.next.next = Node.new(32)
 
-m1 = Node.new(5, nil)
-m2 = Node.new(22, nil)
-m3 = Node.new(25, nil)
-m4 = Node.new(32, nil)
-m1.next = m2
-m2.next = m3
-m3.next = m4
-
-puts "List1"
-print_list(l1)
-
-puts "List2"
-print_list(m1)
-
-# result = merge_inplace(l1, m1)
-
+# puts "List1"
+# print_list(head1)
+# puts "List2"
+# print_list(head2)
+# result = merge_inplace(head1, head2)
 # puts "Printing Result"
 # print_list(result)
 
@@ -79,41 +67,40 @@ print_list(m1)
 #  = Alternate approach =
 #  ======================
 
-def merge_util(l1, l2)
-  curr = l1
-  c2 = l2
-  while curr.next != nil && c2 != nil
-    if curr.next.item > c2.item
-      nxt = curr.next
-      curr.next = c2
-      c2 = c2.next
-      curr.next.next = nxt
+def merge_util(list_a, list_b)
+  curr = list_a
+  _next = curr.next
+
+  while _next && list_b
+    if _next.item <= list_b.item
+      curr = curr.next
+      _next = curr.next
+    else
+      curr.next = list_b
+      list_b = list_b.next
+      curr = curr.next
+      curr.next = _next
     end
-    curr = curr.next
   end
-  if curr.next == nil
-    curr.next = c2
-  end
-  return l1
+
+  curr.next = list_b if list_b
+  list_a
 end
 
-def merge(h1, h2)
-  if h1 == nil and h2 == nil
-    return nil
-  elsif h1 == nil
-    return h2
-  elsif h2 == nil
-    return h1
-  end
 
-  if h1.item < h2.item
-    merge_util(h1, h2)
+def merge(list_a, list_b)
+  if list_a.nil?
+    list_b
+  elsif list_b.nil?
+    list_a
+  elsif list_a.item <= list_b.item
+    merge_util(list_a, list_b)
   else
-    merge_util(h2, h1)
+    merge_util(list_b, list_a)
   end
 end
 
-result = merge(l1, m1)
+result = merge(head1, head2)
 
 puts "Printing Result"
 print_list(result)
