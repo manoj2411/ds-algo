60f0751780871ad0b2d6b7205f72c04f6c923f80
Manoj <manoj.delhi24@gmail.com>
2020-01-27 07:26:51 +0100

    Refactoring : subarray with given sum, adding comments for explaining algorith conditions.

--- Array/subarray_for_sum_non-nag.rb
+++ Array/subarray_for_sum_non-nag.rb
@@ -1,16 +1,19 @@
-def subarray_with_sum(arr, count)
+def subarray_with_sum(arr, given_sum)
   start = 0
   i = 1
   sum = arr[start]
 
+  # since the value of sum is adjusted after comparing the given_sum, we need 1 more
+  #  iteration to reach to the sum & given_sum comparision.
   while i <= arr.length
-
-    while sum > count && start < (i - 1)
+    #  start < (i - 1) making sure that we are not removing all the items from sum
+    #   with this there will be always a gap of at least 1 in start & i
+    while sum > given_sum && start < (i - 1)
       sum -= arr[start]
       start += 1
     end
 
-    if count == sum
+    if given_sum == sum
       puts "The lies between #{start} and #{i - 1 }"
       return true
     end
@@ -24,11 +27,22 @@ def subarray_with_sum(arr, count)
   puts "No subarray found! "
 end
 
-# arr = [10,2,1,4,20]
-# sum = 6
-arr = [1, 4, 20, 3, 10, 5]
-sum = 23
-# arr = [1, 4, 0, 0, 3, 10, 5]
-# sum = 20
-puts "Arr: #{arr}, sum: #{sum}"
-subarray_with_sum(arr, sum)
+
+
+for arr, sum in [
+  [[1,2,3,7,5], 12], # [1,3]
+  [[1,2,3,7,5], 15], # [2,4]
+  [[1,2,3,7,5], 27], # -1
+  [[1,2,3,7,5], 2],  # [1,1]
+  [[1,2,3,4,5,6,7,8,9,10], 15], # [0,4]
+  [[1,4,20,3,10,5], 33], # [2,4]
+  [[1,4,0,0,3,10,5],7], # [1,4]
+  [[2,4], 1], # -1
+  [[1,4], 0], # -1
+
+]
+  puts "Arr: #{arr}, sum: #{sum}"
+  subarray_with_sum(arr, sum)
+  puts
+end
+
