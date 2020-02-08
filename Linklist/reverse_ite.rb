c75d9ffd11614b30a23fc0820eea1b0a0790dda5
Manoj <manoj.delhi24@gmail.com>
2020-02-08 09:19:51 +0100

    refactoring reverse linked list iterative solution

--- Linklist/reverse_ite.rb
+++ Linklist/reverse_ite.rb
@@ -1,16 +1,25 @@
 Node = Struct.new(:item, :next)
-def rev(node)
-  curr = node
-  start = nil
-  while curr != nil
-    tmp = curr
-    curr = curr.next
-    tmp.next = start
-    start = tmp
-  end
 
-  return start
+def reverse(node)
+  curr = node
+  prev = nil
+  while curr
+    nxt = curr.next
+    curr.next = prev
+    prev = curr
+    curr = nxt
+  end
+  prev
 end
+
+def print_list(node)
+  while node != nil
+    print "#{node.item} "
+    node = node.next
+  end
+  puts
+end
+
 
 head = Node.new(1)
 head.next = Node.new(3)
@@ -19,18 +28,8 @@ head.next.next.next = Node.new(7)
 head.next.next.next.next = Node.new(8)
 head.next.next.next.next.next = Node.new(9)
 
-def to_p(head)
-  curr = head
-  while curr != nil
-    print "#{curr.item} ->"
-    curr = curr.next
-  end
-  puts
-end
-
-print "Old List : "
-to_p(head)
-head = rev(head)
-puts
-print "New List : "
-to_p(head)
+print "Original : "
+print_list(head)
+head = reverse(head)
+print "Result : "
+print_list(head)
