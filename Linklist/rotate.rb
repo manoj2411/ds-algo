6f7fe3c0092bded443804930ea9a76b57c5b0804
Manoj <manoj.delhi24@gmail.com>
2020-03-19 07:18:52 +0100

    refactoring rotate linked list by K

--- Linklist/rotate.rb
+++ Linklist/rotate.rb
@@ -1,21 +1,22 @@
 Node = Struct.new(:item, :next)
 
 def rotate_by_k(head, k)
+  return head if head.nil?
+
   curr = head
   i = 1
-  tmp = nil
-  while curr.next != nil
-    tmp = curr if i == k
+  rotation_point = nil
+  while curr.next
+    rotation_point = curr if i == k
     i += 1
     curr = curr.next
   end
 
-  if tmp
+  if rotation_point # it can be nil in case of k == Len(list) OR list has only 1 node. In this case nothing needs to be changed
     curr.next = head
-    head = tmp.next
-    tmp.next = nil
+    head = rotation_point.next
+    rotation_point.next = nil
   end
-
   head
 end
 
@@ -29,9 +30,14 @@ end
 
 head = Node.new(2)
 head.next = Node.new(4)
-head.next.next = Node.new(7)
-head.next.next.next = Node.new(8)
-head.next.next.next.next = Node.new(9)
+# head.next.next = Node.new(7)
+# head.next.next.next = Node.new(8)
+# head.next.next.next.next = Node.new(9)
+# puts "Original list"
+# pp head
+# head = rotate_by_k(head, 2)
+# puts "Rotated list"
+# pp head
 
 head2 = Node.new(1)
 head2.next = Node.new(2)
@@ -42,18 +48,11 @@ head2.next.next.next.next.next = Node.new(6)
 head2.next.next.next.next.next.next = Node.new(7)
 head2.next.next.next.next.next.next.next = Node.new(8)
 
-
-puts "Original list"
-pp head
-head = rotate_by_k(head, 3)
-puts "Rotated list"
-pp head
-
-puts "Original list"
-pp head2
-head2 = rotate_by_k(head2, 4)
-puts "Rotated list"
-pp head2
+# puts "Original list"
+# pp head2
+# head2 = rotate_by_k(head2, 4)
+# puts "Rotated list"
+# pp head2
 
 head2.next.next = nil
 puts "Original list"
