577b1b812e37478fb0341919165bd31524a6cd61
Manoj <manoj.delhi24@gmail.com>
2020-03-22 09:54:52 +0100

    adding itertaive method to mirror a binary tree

--- Tree/mirror_tree.rb
+++ Tree/mirror_tree.rb
@@ -32,3 +32,17 @@ mirror(root)
 
 puts "Preorder(mirrored):"
 puts print_preorder(root) # 4  5  2  3  1
+
+
+# iterative method
+def mirror_iteratively(root)
+    que = Queue.new
+    que.enq(root)
+    while not que.empty?
+        node = que.deq
+        node.left, node.right = node.right, node.left
+        que.enq(node.left) if node.left
+        que.enq(node.right) if node.right
+    end
+    root
+end
