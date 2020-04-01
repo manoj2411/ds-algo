cc6b4ad9e03512794e974f06b0d386d21e7d1971
Manoj <manoj.delhi24@gmail.com>
2020-04-01 13:52:20 +0100

    clean up for balanced parenthesis

--- String/balanced_parenthesis.rb
+++ String/balanced_parenthesis.rb
@@ -9,8 +9,6 @@
 
 def is_balanced?(str)
   stack = []
-  open_parns = ['[', '(', '{']
-  closing_parns = [']', ')', '}']
 
   str.each_char do |chr|
     if chr == '(' || chr == '{' || chr == '['
@@ -26,13 +24,18 @@ def is_balanced?(str)
       return false
     end
   end
-  return stack.empty?
+
+  stack.empty?
 end
 
-# str = "a(bcd)d"
-# str = "(kjds(hfkj)sdhf"
-# str = "(sfdsf)(fsfsf"
-# str = "{[]}()"
-# str = "{[}]"
 
-puts "String: #{str} \nis_balanced? #{is_balanced?(str)}"
+for str in [
+  "a(bcd)d", # true
+  "(kjds(hfkj)sdhf", # false
+  "(sfdsf)(fsfsf", # false
+  "{[]}()", # true
+  "{[}]", # false
+]
+
+  puts "is_balanced? #{is_balanced?(str)} \t #{str} "
+end
