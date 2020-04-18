a7cb2c07dc78efd5fb7c9661ce34079268aacd3e
Manoj <manoj.delhi24@gmail.com>
2020-04-18 07:47:01 +0100

    solution for Interval List Intersections

---
+++ Array/interval_intersection.rb
@@ -0,0 +1,33 @@
+def interval_intersection(a, b)
+    i1 = i2 = 0
+    result = []
+
+    while i1 < a.length && i2 < b.length
+        s1, e1 = a[i1]
+        s2, e2 = b[i2]
+
+        if s1 > e2
+            i2 += 1
+            next
+        elsif s2 > e1
+            i1 += 1
+            next
+        end
+
+        result << [
+            [s1,s2].max,
+            [e1,e2].min
+        ]
+
+        if e1 > e2
+            i2 += 1
+        else
+            i1 += 1
+        end
+    end
+    result
+end
+
+a = [[0,2],[5,10],[13,23],[24,25]]
+b = [[1,5],[8,12],[15,24],[25,26]]
+puts interval_intersection(a, b).to_s # [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
