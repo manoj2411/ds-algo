5e8ffd18d0ae1c6c0760eba892f20b3d2e3becbe
Manoj <manoj.delhi24@gmail.com>
2020-06-06 12:41:44 +0200

    Solution for Target Sum problem (brute force)

---
+++ Dp/target_sum_brute_force.rb
@@ -0,0 +1,20 @@
+# Given an array of integers and a target value. Find the number of ways that you can add and subtract the values in nums to add up to T.
+#
+def target_sum(nums, target)
+  helper(nums, target, 0, 0)
+end
+
+def helper(a, target, sum, i)
+  if i == a.length - 1
+    (sum + a[i] == target || sum - a[i] == target) ? 1 : 0
+  else
+    helper(a, target, sum + a[i], i + 1) +
+    helper(a, target, sum - a[i], i + 1)
+  end
+end
+
+for arr, target in [
+  [[1,1,1,1,1], 3], # 5
+]
+  puts target_sum(arr, target)
+end
