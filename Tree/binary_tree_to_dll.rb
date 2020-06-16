fadd254e999310f113e9464a1421f769a4df075f
Manoj <manoj.delhi24@gmail.com>
2020-06-16 07:46:46 +0200

    refactoring binary tree to Doubly link list code

--- Tree/binary_tree_to_dll.rb
+++ Tree/binary_tree_to_dll.rb
@@ -1,37 +1,38 @@
 Node = Struct.new(:key, :left, :right)
 module BinaryTree
   module_function
-  Store = Struct.new(:curr)
 
-  def inorder(node, store)
+  def inorder(node, prev)
     return if node == nil
 
-    inorder(node.left, store)
+    inorder(node.left, prev)
     # print "#{node.key}.prev(l) = #{store.curr.key} .. "
-    node.left = store.curr
+    node.left = prev[0]
     # print "#{store.curr.key}.next(r) = #{node.key} .. "
-    store.curr.right = node
+    prev[0].right = node
     # print "curr is #{store.curr.key}\n"
-    store.curr = store.curr.right
-    inorder(node.right, store)
+    prev[0] = node
+    inorder(node.right, prev)
 
   end
 
   def to_dll(root)
     dummy = Node.new('dummy')
-    store = Store.new(dummy)
+    store = [dummy]
     inorder(root, store)
-    return dummy
+    dummy.right.left = nil
+    return dummy.right
   end
 
 end
 
 def print_list(root)
-  curr = root.right
-  while curr != nil
-    puts "left: #{curr.left.key if curr.left} .. key: #{curr.key} .. right #{curr.right.key if curr.right}"
-    curr = curr.right
+  while root != nil
+    # puts "left: #{curr.left.key if curr.left} .. key: #{curr.key} .. right #{curr.right.key if curr.right}"
+    print "#{root.key} "
+    root = root.right
   end
+  puts
 end
 
 root = Node.new(10)
@@ -41,6 +42,6 @@ root.left.left = Node.new(25)
 root.left.right = Node.new(30)
 root.right.left = Node.new(36)
 
-root = BinaryTree.to_dll(root)
+head = BinaryTree.to_dll(root)
 
-print_list(root)
+print_list(head)
