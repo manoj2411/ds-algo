2fb92f5526867444bb4277f30ae74111623d4e8b
Manoj <manoj.delhi24@gmail.com>
2020-07-20 11:53:59 +0200

    refactoring print left view code

--- Tree/print_left_view.rb
+++ Tree/print_left_view.rb
@@ -1,30 +1,24 @@
 Node = Struct.new(:key, :left, :right)
 
-def print_left_view(node, arr, level)
-  return if node.nil?
-
-  if arr[level] == nil
-    print " #{node.key} "
-    arr[level] = true
-  end
-  print_left_view(node.left, arr, level + 1)
-  print_left_view(node.right, arr, level + 1)
+def print_left_view(node)
+  print_left_view_helper(node, [0], 1)
+  puts
 end
 
-@max_level = -1
-def print_left_view_new(node, level)
+def print_left_view_helper(node, max_level, curr_level)
   return if node.nil?
 
-  if @max_level < level
-    print " #{node.key} "
-    @max_level = level
+  if max_level[0] < curr_level
+    print "#{node.key} "
+    max_level[0] = curr_level
   end
-  print_left_view_new(node.left, level + 1)
-  print_left_view_new(node.right, level + 1)
+
+  print_left_view_helper(node.left, max_level, curr_level + 1)
+  print_left_view_helper(node.right, max_level, curr_level + 1)
 end
 
 
-# Node.new()
+
 
 root = Node.new(4)
 root.left = Node.new(5)
@@ -36,4 +30,13 @@ root.right.right.left.right = Node.new(22)
 root.right.left.left = Node.new(6)
 root.right.left.right = Node.new(7)
 
-print_left_view_new(root, 0)
+print_left_view(root) # 4 5 3 6 22
+
+root = Node.new(1)
+root.left = Node.new(2)
+root.right = Node.new(3)
+
+root.left.right = Node.new(4)
+root.left.right.right = Node.new(5)
+root.left.right.right.right = Node.new(6)
+print_left_view(root) # 1 2 4 5 6
