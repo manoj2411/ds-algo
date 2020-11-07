62aaea6254a55b0298c0441bae342e9c158bf889
Manoj <manoj.delhi24@gmail.com>
2020-11-07 14:44:52 +0100

    refactoring roman_to_int

--- String/roman_to_int.py
+++ String/roman_to_int.py
@@ -1,12 +1,10 @@
 def roman_to_int(str):
     mapping = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    right = len(str) - 1
-    number = mapping[str[right]]
-    prev = number
+    number = 0
+    prev = 0
 
-
-    for i in range(right - 1, -1, -1):
+    for i in range(len(str) - 1, -1, -1):
         curr = mapping[str[i]]
 
         if curr < prev :
