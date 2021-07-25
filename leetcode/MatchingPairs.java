a16ffd7bb41fcb1bed428c5c39d4c4181a4f9961
Manoj Kumar <sehrawat.manoj24@gmail.com>
2021-07-25 08:05:14 +0200

    Add new impl of Matching Pairs problem to handle more cases

--- leetcode/MatchingPairs.java
+++ leetcode/MatchingPairs.java
@@ -4,89 +4,152 @@ import java.util.*;
 	https://leetcode.com/discuss/interview-question/632717/Facebook-or-Recruiting-Portal-or-Matching-Pairs
 */
 class MatchingPairs {
+
 	public static void main(String[] args) {
+		List<List<String>> tests = new ArrayList<>();
+		List<Integer> expected = new ArrayList<>();
+
+		// diff == 0
+		tests.add(List.of("abc", "abc"));
+		expected.add(1);
+		tests.add(List.of("abcb", "abcb"));
+		expected.add(4);
+
+		// diff == 1
+		tests.add(List.of("abc", "abx"));
+		expected.add(1);
+		tests.add(List.of("abc", "abb"));
+		expected.add(2);
+		tests.add(List.of("aac", "aax"));
+		expected.add(2);
+		tests.add(List.of("aacd", "aadd"));
+		expected.add(3);
+
+		// diff > 1
+		tests.add(List.of("abc", "bca"));
+		expected.add(1);
+		tests.add(List.of("abcd", "adcb"));
+		expected.add(4);
+		tests.add(List.of("abcde", "bcade"));
+		expected.add(3);
+		tests.add(List.of("abcde", "baade"));
+		expected.add(4);
+		tests.add(List.of("anopab", "apoqba"));
+		expected.add(4);
 
 
-		System.out.println(new Solution().matchingPairs("abc", "bca")); // 1
+		for(int i = 0; i < tests.size(); i++) {
 
-		System.out.println(new Solution().matchingPairs("anopab", "apoqba")); // 4
+			String s = tests.get(i).get(0);
+			String t = tests.get(i).get(1);
+			int output = new Solution().matchingPairs(s, t);
 
-		System.out.println(new Solution().matchingPairs("abcd", "adcb")); // 4
-		System.out.println(new Solution().matchingPairs("abcde", "bcade")); // 3
-		System.out.println(new Solution().matchingPairs("abcde", "baade")); // 4
+			check(output, expected.get(i), i + 1);
 
-		System.out.println(new Solution().matchingPairs("abc", "abc")); // 1
-		System.out.println(new Solution().matchingPairs("abcde", "baade")); // 4
+		};
 	}
+
+	static void check(int output, int expected, int test_case_number) {
+
+    	boolean result = (expected == output);
+    	char rightTick = '\u2713';
+    	char wrongTick = '\u2717';
+
+    	if (result) {
+      		System.out.println(rightTick + " Test #" + test_case_number);
+    	} else {
+      		System.out.print(wrongTick + " Test #" + test_case_number + ": Expected ");
+      		printInteger(expected);
+      		System.out.print(" Your output: ");
+      		printInteger(output);
+      		System.out.println();
+    	}
+    	test_case_number++;
+  }
+
+  static void printInteger(int n) {
+    System.out.print("[" + n + "]");
+  }
+
 }
+
 
 class Solution {
-	List<List<Integer>> diffCharIndices = buildDiffCharIndices();
 
-	int matchingPairs(String s, String t) {
+  boolean containsUniqueChars(String str) {
+    Set<Character> set = new HashSet<>();
 
-		int matched = 0;
+    for(int i = 0; i < str.length(); i++) {
+      if (!set.add(str.charAt(i))) return false;
+    }
+    return true;
+  }
 
-		for(int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == t.charAt(i)) {
-				matched++;
-			} else {
-				int ch = s.charAt(i) - 'a';
-				diffCharIndices.get(ch).add(i);
-			}
-		}
+  // if ch present in str not at the given index
+  boolean containsCharNotAt(String str, char ch, int index) {
+    for(int i = 0; i < str.length(); i++) {
+      if (index == i) continue;
+      if (str.charAt(i) == ch) return true;
+    }
+    return false;
+  }
 
-		if (matched == s.length()) return matched - 2; // everything is matching
+  int gainAfterBestMatch(String s, String t, List<Integer> diff_ids, Map<Character, List<Integer>>diff_map) {
+    boolean foundMatch = false;
 
-		if (canSwapWithAtLeastOneMatch(t)) {
-			matched++;
-		} else {
-			return matched; // no replaceable match in missing characters
-		}
+    for(int i : diff_ids) {
+      char ch_in_t = t.charAt(i);
 
-		return canSwapWithTwoMatch(s, t) ? matched + 1 : matched;
-	}
+      if (diff_map.containsKey(ch_in_t)) {
+        foundMatch = true;
+        for(int j : diff_map.get(ch_in_t)) {
+        	// find a perfect swap
+          	if (t.charAt(j) == s.charAt(i))
+            	return 2;
+        }
+      }
+    }
+
+    return foundMatch ? 1 : 0;
+  }
 
 
-	private boolean canSwapWithTwoMatch(String s, String t) {
+  int matchingPairs(String s, String t) {
+    int N = s.length();
+    List<Integer> diff_ids = new ArrayList<>();
+    Map<Character, List<Integer>>diff_map = new HashMap<>();
 
-		for(int i = 0; i < 26; i++) {
-			for(int s_i : diffCharIndices.get(i)) {
+    for(int i = 0; i < N; i++) {
+      char ch_in_s = s.charAt(i);
 
-				int ch_t = t.charAt(s_i) - 'a';
-				// System.out.println("s(i) : " + s.charAt(s_i) + ", t(i): " + t.charAt(s_i));
+      if(ch_in_s != t.charAt(i)) {
+        diff_ids.add(i);
 
-				for (int s_j : diffCharIndices.get(ch_t)) {
-					// System.out.println("  s(j) : " + s.charAt(s_j));
-					if (t.charAt(s_j) == s.charAt(s_i))
-						return true; // there is a matching switch possible
-				}
-			}
-		}
+        if (!diff_map.containsKey(ch_in_s))
+          diff_map.put(ch_in_s, new ArrayList<>());
 
-		return false;
-	}
+        diff_map.get(ch_in_s).add(i);
+      }
+    }
 
-	private boolean canSwapWithAtLeastOneMatch(String t) {
-		for(int i = 0; i < 26; i++) {
-			for(int indx : diffCharIndices.get(i)) {
+    int num_of_matches = N - diff_ids.size();
 
-				int char_in_t = t.charAt(indx) - 'a';
+    if (diff_ids.size() == 0) {
+    	// s contains duplicates means we can swap without loosing matching count
+      	return containsUniqueChars(s) ? num_of_matches - 2 : num_of_matches;
+    }
 
-				if (diffCharIndices.get(char_in_t).size() > 0)
-					return true;
-			}
-		}
+    if (diff_ids.size() == 1) {
+      int i = diff_ids.get(0);
+      // contains duplicates means we can swap without loosing matching count
+      if (!containsUniqueChars(s)) return num_of_matches;
+      if (containsCharNotAt(s, t.charAt(i), i)) return num_of_matches;
+      return num_of_matches - 1;
+    }
 
-		return false;
-	}
+    // maximize the matches by 1 or by 2
+    return num_of_matches + gainAfterBestMatch(s, t, diff_ids, diff_map);
+  }
 
-	private List<List<Integer>> buildDiffCharIndices() {
-		List<List<Integer>> diffCharIndices = new ArrayList<>();
-
-		for(int i = 0; i < 26; i++)
-			diffCharIndices.add(new ArrayList<>());
-
-		return diffCharIndices;
-	}
 }
+
