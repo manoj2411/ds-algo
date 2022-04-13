7df20ee0ef36e979b9f462de1643e4bf2902e437
Manoj Kumar <sehrawat.manoj24@gmail.com>
2022-04-13 11:53:18 +0200

    Add alternate solution of Merge Intervals problem

--- leetcode/MergeIntervals.java
+++ leetcode/MergeIntervals.java
@@ -62,3 +62,27 @@ class Solution {
         return result.toArray(new int[result.size()][]);
     }
 }
+
+class AlternateSolution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+
+        List<int[]> result = new ArrayList<>();
+
+        result.add(intervals[0]);
+
+        for(int i = 1; i < intervals.length; i++) {
+            int lastIndex = result.size() - 1;
+            int[] last = result.get(lastIndex);
+            int[] curr = intervals[i];
+
+            if (curr[0] <= last[1]) { // merge
+                result.get(lastIndex)[1] = Math.max(last[1], curr[1]);
+            } else { // add new entry
+                result.add(curr);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
