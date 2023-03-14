9582bc2ca06752393b81272b91a6128b2d2b3f94
Manoj <manoj.delhi24@gmail.com>
2023-03-14 09:33:21 +0530

    Add alternate solution for Sum Root to Leaf Numbers problem

--- leetcode/SumRootToLeafNumbers.java
+++ leetcode/SumRootToLeafNumbers.java
@@ -9,11 +9,34 @@ class SumRootToLeafNumbers {
 		root.left.right = new TreeNode(1);
 		root.right = new TreeNode(0);
 
-		System.out.println(new Solution().sumNumbers(root)); // 1026
+		System.out.println(new SolutionWithIntPath().sumNumbers(root)); // 1026
 	}
 }
 
-class Solution {
+
+class SolutionWithIntPath {
+    public int sumNumbers(TreeNode root) {
+        return helper(root, 0);
+    }
+
+    private int helper(TreeNode node, int path) {
+        if (node == null) {
+            return 0;
+        }
+
+        int newPath = (path * 10) + node.val;
+        if (isLeaf(node))  {
+            return newPath;
+        }
+
+        return helper(node.left, newPath) + helper(node.right, newPath);
+    }
+    private boolean isLeaf(TreeNode node) {
+        return node.left == null && node.right == null;
+    }
+}
+
+class SolutionWithStrPath {
     int sum = 0;
 
     public int sumNumbers(TreeNode root) {
