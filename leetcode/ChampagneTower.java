310e7bebadbe696b4625c2df6afa522830fc72fc
Manoj Kumar <sehrawat.manoj24@gmail.com>
2023-09-24 14:36:05 +0200

    Add space optimised solution of Champagne Tower problem

--- leetcode/ChampagneTower.java
+++ leetcode/ChampagneTower.java
@@ -3,7 +3,7 @@
 */
 class ChampagneTower {
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        AlternativeSolution sol = new AlternativeSolution();
         System.out.println(sol.champagneTower(1,1,1)); // 0
         System.out.println(sol.champagneTower(2,1,1)); // 0.5
         System.out.println(sol.champagneTower(100000009,33,17)); // 1.0
@@ -25,5 +25,26 @@ class Solution {
             }
         }
         return store[query_row][query_glass];
+    }
+}
+
+class AlternativeSolution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[] currRow = new double[] {poured};
+
+        for(int row = 1 ;row <= query_row; row++) {
+            double[] nextRow = new double[row+1];
+
+            for(int i = 0; i < currRow.length; i++) {
+                if(currRow[i] > 1) {
+                    double extra = (currRow[i] - 1) / 2;
+                    nextRow[i] += extra;
+                    nextRow[i+1] += extra;
+                }
+            }
+            currRow = nextRow;
+        }
+
+        return currRow[query_glass] > 1 ? 1 : currRow[query_glass];
     }
 }
